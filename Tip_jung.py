c181e0d39b6450d8c81df63856ad464a123973b3
Balgeum Cha <cbg111580@ewhain.net>
2021-05-09 22:44:54 +0900

    initial commit

---
+++ Tip_jung.py
@@ -0,0 +1,19 @@
+"""
+가상환경 설정
+    Ctrl + p
+    >Python: Select Interpreter
+    'py38': conda 고르기
+"""
+
+"""
+# %%
+실행: Shift + Enter
+"""
+
+
+
+"""shift + v : visual line 모드
+ctrl + v: visual block 모드"""
+
+""": w 저장, 
+: wq 저장 후 종료"""
